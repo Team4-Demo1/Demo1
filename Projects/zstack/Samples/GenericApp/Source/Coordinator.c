434f92b83eaeca61a9947572811a5ec0cec67b6f
kily-03 <549474980@qq.com>
2021-05-18 19:13:25 +0800

    Coordinator.h

--- Projects/zstack/Samples/GenericApp/Source/Coordinator.c
+++ Projects/zstack/Samples/GenericApp/Source/Coordinator.c
@@ -1,156 +1,134 @@
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-* 文件名  ：Coordinator
-* 作者    ：wengzhen
-* 版本    ：V0.0.1
-* 时间    ：2021/5/18
-* 描述    ：协调器的主函数
+* 文件名  ： Coordinator
+* 作者    ： Liuyibing
+* 版本    ： V0.0.1
+* 时间    ： 2021/5/18 星期二
+* 描述    ： 协调器接受数据并向云端发送数据
 ********************************************************************
 * 副本
 *
 *>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-
 /* 头文件 ----------------------------------------------------------------*/
-#include "oSAL.h"
-#include "AF.h"
-#include "ZDApp.h"
-#include "ZDObject.h"
-#include "ZDProfile.h"
-#include <string.h>
-#include "Coordinator.h"
-#include "DebugTrace.h"
-#if !defined ( WIN32 )
+#include "Coordinator.h"     
+#include  "OSAL.h"
+#include  "AF.h"
+#include  "ZDAPP.h"
+#include  "ZDObject.h"
+#include  "ZDProfile.h"
+#include  <string.h>
+#include  "Coordinator.h"
+#include  "DebugTrace.h"
+#if !defined(WIN32)
 #include "OnBoard.h"
 #endif
 #include "hal_lcd.h"
 #include "hal_led.h"
 #include "hal_key.h"
 #include "hal_uart.h"
+
+/* 宏定义 ----------------------------------------------------------------*/
+/* 结构体或枚举 ----------------------------------------------------------*/
 const cId_t GenericApp_ClusterList[GENERICAPP_MAX_CLUSTERS] =
 {
-    GENERICAPP_CLUSTERID//在Coordinator.h文件中定义的宏，为了跟协议栈里面的数据保持一致
+    GENERICAPP_CLUSTERID
 };
-//描述一个ZigBee设备节点
 const SimpleDescriptionFormat_t GenericApp_SimpleDesc =
 {
-    GENERICAPP_ENDPOINT,              //  int Endpoint;
-    GENERICAPP_PROFID,                //  uint16 AppProfId[2];
-    GENERICAPP_DEVICEID,              //  uint16 AppDeviceId[2];
-    GENERICAPP_DEVICE_VERSION,        //  int   AppDevVer:4;
-    GENERICAPP_FLAGS,                 //  int   AppFlags:4;
-    GENERICAPP_MAX_CLUSTERS,          //  byte  AppNumInClusters;
-    (cId_t *)GenericApp_ClusterList,  //  byte *pAppInClusterList;
+    GENERICAPP_ENDPOINT,
+    GENERICAPP_PROFID,
+    GENERICAPP_DEVICEID,
+    GENERICAPP_DEVICE_VERSION,
+    GENERICAPP_FLAGS,
+    GENERICAPP_MAX_CLUSTERS,
+    (cId_t *)GenericApp_ClusterList,
     0,
-    (cId_t *)NULL                     //  byte *pAppInClusterList;
+    (cId_t *)NULL
 };
 
+endPointDesc_t GenericApp_epDesc;
+byte GenericApp_TaskID;
+byte GenericApp_TransID;
+//unsigned char uartbuf[128];
 
-endPointDesc_t GenericApp_epDesc;   // 节点描述符
-byte GenericApp_TaskID;             // 任务优先级
-byte GenericApp_TransID;            // 数据发送序列号
-/* 宏定义 ----------------------------------------------------------------*/
-/* 结构体或枚举 ----------------------------------------------------------------*/
-/* 内部函数声明 ----------------------------------------------------------------*/
+/* 内部函数声明-----------------------------------------------------------*/
+void GenericApp_MessageMSGCB(afIncomingMSGPacket_t *pckt );
+void GenericApp_SendTheMessage(void);
+//static void rxCB (uint8 port,uint8 event);
 
-
-
-
-
-
-/* 函数 ----------------------------------------------------------------*/
-
-/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-* 函数名  ：GenericApp
-* 参数    ：byte  task_id
-* 返回    ：void
-* 作者    ：wengzhen
-* 时间    ：2021/5/18
-* 描述    ：任务初始化函数
-----------------------------------------------------------------*/
-void  GenericApp_Init(byte  task_id)
+/* 函数 ------------------------------------------------------------------*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名  ： GenericApp_Init 
+* 参数    ： byte task_id
+* 返回    ： void
+* 作者    ： Liuyibing
+* 时间    ： 2021/5/18 星期二
+* 描述    ： 定义id
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void GenericApp_Init(byte task_id)
 {
-    GenericApp_TaskID = task_id;    // 初始化任务优先级
-    GenericApp_TransID = 0;         //将发送数据序列号初始化为0
+    //    halUARTCfg_t uartConfig;
+    GenericApp_TaskID              =task_id;
+    GenericApp_TransID             = 0;
+    GenericApp_epDesc.endPoint     =GENERICAPP_ENDPOINT;
+    GenericApp_epDesc.task_id      =&GenericApp_TaskID;
+    GenericApp_epDesc.simpleDesc   =(SimpleDescriptionFormat_t *)&GenericApp_SimpleDesc;
+    GenericApp_epDesc.latencyReq   =noLatencyReqs;
+    afRegister( &GenericApp_epDesc);
     
-    GenericApp_epDesc.endPoint = GENERICAPP_ENDPOINT;
-    GenericApp_epDesc.task_id = &GenericApp_TaskID;
-    GenericApp_epDesc.simpleDesc = (SimpleDescriptionFormat_t *)&GenericApp_SimpleDesc;
-    GenericApp_epDesc.latencyReq = noLatencyReqs;   //对节点描述符进行初始化
-    afRegister( &GenericApp_epDesc);                //使用afRegister对节点描述符进行注册
-    
-    
-    
+    /*    uartConfig.configured   =   TRUE;
+    uartConfig.baudRate   =   HAL_UART_BR_115200;
+    uartConfig.flowControl    =   FALSE;
+    uartConfig.callBackFunc   =   rxCB;
+    HalUARTOpen (0, &uartConfig);*/
 }
-/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-* 函数名  ：GenericApp_ProcessEvent
-* 参数    ：byte task_id, UINT16 events
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名  ： GenericApp_ProcessEvent
+* 参数    ：  byte task_id, UINT16 events
 * 返回    ： UINT16
-* 作者    ：wengzhen
-* 时间    ：2021/5/18
-* 描述    ：对接收数据的处理函数
-----------------------------------------------------------------*/
-UINT16  GenericApp_ProcessEvent(byte task_id, UINT16 events)
+* 作者    ： Liuyibing
+* 时间    ： 2021/5/18 星期二
+* 描述    ： 函数说明
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events )
 {
-    afIncomingMSGPacket_t *MSGpkt;//定义一个接收信息结构体的指针
+    afIncomingMSGPacket_t *MSGpkt;
     if ( events & SYS_EVENT_MSG )
     {
-        //osal_msg_receive函数从消息列队上接受信息
-        MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( GenericApp_TaskID );
+        MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive(GenericApp_TaskID);
         while ( MSGpkt )
         {
-            switch( MSGpkt->hdr.event )
+            switch ( MSGpkt->hdr.event )
             {
-                //判断接收信息是否为无线数据
             case AF_INCOMING_MSG_CMD:
-                GenericApp_MessageMSGCB ( MSGpkt );//对数据进行处理
+                GenericApp_MessageMSGCB( MSGpkt );
                 break;
             default:
                 break;
             }
-            //释放消息向上的储存空间
-            osal_msg_deallocate( (uint8 *)MSGpkt );
-            
-            //处理完第一个消息之后,接着处理下一个消息,直到完成
-            MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( GenericApp_TaskID );
-            
-        } 
-        //返回未完成的信息
-        return (events ^ SYS_EVENT_MSG);
-        
+            osal_msg_deallocate( (uint8 *) MSGpkt );
+            MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive
+                ( GenericApp_TaskID );
+        }
+        return (events^SYS_EVENT_MSG);
     }
-    return 0;
+    return 0 ;
 }
-
-/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-* 函数名  ：GenericApp_MessageMSGCB
-* 参数    ：afIncomingMSGPacket_t *pkt
-* 返回    ：void
-* 作者    ：wengzhen
-* 时间    ：2021/5/18
-* 描述    ：消息处理函数
-----------------------------------------------------------------*/
-void GenericApp_MessageMSGCB ( afIncomingMSGPacket_t *pkt )
+void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pkt)
 {
-    unsigned char buffer[4] = "   ";
-    switch ( pkt -> clusterId )
+    unsigned char buffer[4]=" ";
+    switch ( pkt->clusterId )
     {
     case GENERICAPP_CLUSTERID:
-        //将接收数据复制到缓冲区
-        osal_memcpy (buffer,pkt->cmd.Data,3);
-        //判断接收数据是否为led
-        if ( (buffer[0] =='L') || (buffer[1] -='E') || (buffer[2]=='D'))
+        osal_memcpy(buffer,pkt->cmd.Data,3);
+        if((buffer[0]=='L')||(buffer[1]=='E')||(buffer[2]=='D'))
         {
-            HalLedBlink(HAL_LED_2,0,50,500);//结果为是的话,led2闪烁, HalLedBlink闪烁
+            HalLedBlink(HAL_LED_2,0,50,500);
         }
         else
         {
-            HalLedSet(HAL_LED_2,HAL_LED_MODE_ON);//结果为不是的话,led2点亮,HalLedBlink电路
+            HalLedSet(HAL_LED_2,HAL_LED_MODE_ON);
         }
-        break ;
+        break;
         
     }
-    
 }
--- Projects/zstack/Samples/GenericApp/Source/Enddevice.c
+++ Projects/zstack/Samples/GenericApp/Source/Enddevice.c
@@ -1,89 +1,152 @@
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+* 文件名  ：Enbddevice
+* 作者    ：Liuyibing
+* 版本    ：V0.0.1
+* 时间    ：2021/5/18
+* 描述    ：文件说明
+********************************************************************
+* 副本
+*
+*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+/* 头文件 ----------------------------------------------------------------*/
 #include "OSAL.h"
 #include "AF.h"
 #include "ZDApp.h"
-#include "ZDObject.h"
+#include "ZDOBject.h"
 #include "ZDProfile.h"
 #include <string.h>
 #include "Coordinator.h"
 #include "DebugTrace.h"
-
-#if !defined( WIN32 )
+#if !defined( WIN32)
 #include "OnBoard.h"
 #endif
-
-
 #include "hal_lcd.h"
 #include "hal_led.h"
 #include "hal_key.h"
 #include "hal_uart.h"
-const cId_t GenericApp_ClusterList[GENERICAPP_MAX_CLUSTERS] =
+/* 宏定义 ----------------------------------------------------------------*/
+
+
+/* 结构体或枚举 ----------------------------------------------------------------*/
+/* 内部函数声明 ----------------------------------------------------------------*/
+void  GenericApp_MessageMSGCB(afIncomingMSGPacket_t *pckt);
+void  GenericApp_SendTheMessage(void);
+/* 函数 ----------------------------------------------------------------*/
+const cId_t GenericApp_ClusterList[GENERICAPP_MAX_CLUSTERS ] = 
 {
     GENERICAPP_CLUSTERID
 };
-const SimpleDescriptionFormat_t GenericApp_SimpleDesc =
+
+const SimpleDescriptionFormat_t GenericApp_SimpleDesc = 
 {
-    GENERICAPP_ENDPOINT,              //  int Endpoint;
-    GENERICAPP_PROFID,                //  uint16 AppProfId[2];
-    GENERICAPP_DEVICEID,              //  uint16 AppDeviceId[2];
-    GENERICAPP_DEVICE_VERSION,        //  int   AppDevVer:4;
-    GENERICAPP_FLAGS,                 //  int   AppFlags:4;
+    GENERICAPP_ENDPOINT,
+    GENERICAPP_PROFID,
+    GENERICAPP_DEVICEID,
+    GENERICAPP_DEVICE_VERSION,
+    GENERICAPP_FLAGS,
     0,
     (cId_t *)NULL,
-    GENERICAPP_MAX_CLUSTERS,          //  byte  AppNumInClusters;
-    (cId_t *)GenericApp_ClusterList,  //  byte *pAppInClusterList;
+    GENERICAPP_MAX_CLUSTERS,
+    (cId_t *)GenericApp_ClusterList
 };
+
 endPointDesc_t GenericApp_epDesc;
 byte GenericApp_TaskID;
-byte GenericApp_TransID; 
+byte GenericApp_TransID;
 devStates_t GenericApp_NwkState;
-void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pckt );
-void GenericApp_SendTheMessage( void );
+
+
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+* 函数名  ：GenericApp_Init
+* 参数    ：byte task_id
+* 返回    ：void
+* 作者    ：Liuyibing
+* 时间    ：2021/5/18
+* 描述    ：函数说明
+----------------------------------------------------------------*/
 void GenericApp_Init( byte task_id )
 {
-    GenericApp_TaskID             = task_id;
-    GenericApp_NwkState           =DEV_INIT;
-    GenericApp_TransID            = 0;
-    GenericApp_epDesc.endPoint    = GENERICAPP_ENDPOINT;
-    GenericApp_epDesc.task_id     = &GenericApp_TaskID;
-    GenericApp_epDesc.simpleDesc  = 
+    GenericApp_TaskID   =   task_id;
+    GenericApp_NwkState   =   DEV_INIT;
+    GenericApp_TransID    =   0;
+    GenericApp_epDesc.endPoint    =   GENERICAPP_ENDPOINT;
+    GenericApp_epDesc.task_id   =   &GenericApp_TaskID;
+    GenericApp_epDesc.simpleDesc    =   
         (SimpleDescriptionFormat_t *)&GenericApp_SimpleDesc;
-    GenericApp_epDesc.latencyReq  = noLatencyReqs;
-    afRegister( &GenericApp_epDesc );
+    GenericApp_epDesc.latencyReq    =   noLatencyReqs;
+    afRegister( &GenericApp_epDesc ); //
 }
+
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+* 函数名  ：GenericApp_ProcessEvent
+* 参数    ：byte task_id,UNIT16 evens
+* 返回    ：UINT16
+* 作者    ：Liuyibing
+* 时间    ：2021/5/18
+* 描述    ：函数说明
+----------------------------------------------------------------*/
 UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events )
 {
     afIncomingMSGPacket_t *MSGpkt;
-    if ( events & SYS_EVENT_MSG )
+    if ( events &SYS_EVENT_MSG )
     {
-        MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( GenericApp_TaskID );
+        MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive
+            (GenericApp_TaskID);
         while ( MSGpkt )
         {
             switch ( MSGpkt->hdr.event )
             {
             case ZDO_STATE_CHANGE:
-                GenericApp_NwkState=(devStates_t)(MSGpkt->hdr.status);
-                if(GenericApp_NwkState==DEV_END_DEVICE)
+                GenericApp_NwkState = (devStates_t)(MSGpkt->hdr.
+                                                    status);
+                if (GenericApp_NwkState ==DEV_END_DEVICE)
                 {
-                    GenericApp_SendTheMessage();
+                    GenericApp_SendTheMessage() ;
                 }
                 break;
             default:
                 break;
             }
             osal_msg_deallocate( (uint8 *)MSGpkt );
-            MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( GenericApp_TaskID );
+            MSGpkt = (afIncomingMSGPacket_t*)osal_msg_receive(GenericApp_TaskID );
         }
         return (events ^ SYS_EVENT_MSG);
     }
     return 0;
 }
-void GenericApp_SendTheMessage(void)
+
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+* 函数名  ：GenericApp_SendTheMessage
+* 参数    ：void
+* 返回    ：void
+* 作者    ：Liuyibing
+* 时间    ：2021/5/18
+* 描述    ：函数说明
+----------------------------------------------------------------*/
+void  GenericApp_SendTheMessage(void)
 {
-    unsigned char theMessageData[4]= "LED";
+    unsigned char theMessageData[4] = "LED";
     afAddrType_t my_DstAddr;
     my_DstAddr.addrMode = (afAddrMode_t)Addr16Bit;
     my_DstAddr.endPoint = GENERICAPP_ENDPOINT;
     my_DstAddr.addr.shortAddr = 0x0000;
-    AF_DataRequest(&my_DstAddr,&GenericApp_epDesc,GENERICAPP_CLUSTERID,3,theMessageData,&GenericApp_TransID,AF_DISCV_ROUTE,AF_DEFAULT_RADIUS);
-    HalLedBlink(HAL_LED_2,0,50,500);
+    AF_DataRequest( &my_DstAddr,&GenericApp_epDesc,
+                   GENERICAPP_CLUSTERID,
+                   3,
+                   theMessageData,
+                   &GenericApp_TransID,
+                   AF_DISCV_ROUTE, 
+                   AF_DEFAULT_RADIUS);
+    HalLedBlink (HAL_LED_2,0,50,500);
+    
+    
 }
+
+
+
+
